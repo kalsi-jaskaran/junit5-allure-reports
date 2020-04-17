081c1f256430d95792150da12325db016933eaaf
Jaskarn <kalsi.jaskaran@gmail.com>
2020-04-17 19:15:39 -0400

    First commit

---
+++ src/test/java/ca/prabjas/juint5/tests/BaseTest.java
@@ -0,0 +1,27 @@
+package ca.prabjas.juint5.tests;
+
+import ca.prabjas.juint5.PageGenerator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public PageGenerator page;
+
+    @BeforeEach
+    public void classLevelSetup() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,15);
+        page = new PageGenerator(driver);
+    }
+
+    @AfterEach
+    public void teardown () {
+        driver.quit();
+    }
+}
---
+++ src/test/java/ca/prabjas/juint5/tests/LoginTest.java
@@ -0,0 +1,46 @@
+package ca.prabjas.juint5.tests;
+
+import ca.prabjas.juint5.pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
+public class LoginTest extends BaseTest {
+
+    @Test
+    @Story("User tries to login the system with invalid username and invalid password.")
+    @Description("Invalid Login Test with Invalid Username and Invalid Password.")
+    public void invalidLoginTest_InvalidUserNameInvalidPassword () {
+        page.getPage(HomePage.class)
+                .GivenIAmAtHomePage()
+                .WhenIGoToLoginPage()
+                .AndIloginToN11("onur@swtestacademy.com", "11223344")
+                .ThenIVerifyPasswordErrorMessage(("E-posta adresiniz veya şifreniz hatalı"));
+    }
+
+    @Test
+    @Story("User tries to login the system with empty username and invalid password.")
+    @Description("Invalid Login Test with Empty Username and Invalid Password.")
+    public void invalidLoginTest_EmptyUserEmptyPassword () {
+        page.getPage(HomePage.class)
+                .GivenIAmAtHomePage()
+                .WhenIGoToLoginPage()
+                .AndIloginToN11("","")
+                .ThenIVerifyLoginUserNameErrorMessage("Lütfen e-posta adresinizi girin.")
+                .ThenIVerifyPasswordErrorMessage("Bu alanın doldurulması zorunludur.");
+    }
+
+    @Test
+    @Story("This is a Fail Story.")
+    @Description("This is a Fail Story Description.")
+    public void failTest () {
+        fail();
+    }
+}
